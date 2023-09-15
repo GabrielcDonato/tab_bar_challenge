13c5e515401b5c90990cd270d832f40e41a721b3
Gabriel Donato <premiersoft-gabriel.donato@ailos.coop.br>
2023-09-15 17:01:15 -0300

    Feat: add elements on page 1

--- tablebarChallenge/Tela01VC.swift
+++ tablebarChallenge/Tela01VC.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class Tela01VC: UIViewController {
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
 
+    @IBOutlet weak var editPictureButton: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func tappedEditPictureButton(_ sender: Any) {
+    }
+    
+    @IBAction func tappedAddButton(_ sender: Any) {
+    }
+    
 }
 
